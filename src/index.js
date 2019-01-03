e331092079ea15ac9576e9ed44f0e21df8e26648
yuvaranei <yperumal2@sapient.com>
2019-01-03 12:02:22 +0530

    Working SASS webpack setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.scss';
 
 const Index = () => {
     return(
-        <div>Hello !!!</div>
+        <div className="main">Hello !!!</div>
     )
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const HtmlWbpackPlugin = require('html-webpack-plugin');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: "./src/index.js",
     output : {
         path : path.resolve(__dirname,'dist'),
-        filename : "basic-setup-bundler.js"
+        filename : "basic-sass-setup-bundler.js"
     },
     mode : "development",
     module :{
@@ -16,10 +17,21 @@ module.exports = {
                 use : {
                     loader : 'babel-loader'
                 }
+            },
+            {       
+                test : /\.(css|scss)$/,
+                exclude : /node_modules/,
+                use : ExtractTextWebpackPlugin.extract({
+                    fallback : 'style-loader',
+                    use : 'css-loader!sass-loader'
+                })
             }
         ]
     },
-    plugins : [new HtmlWbpackPlugin({
+    plugins : [
+        new HtmlWbpackPlugin({
         template : './src/index.html'
-    })]
+        }),
+        new ExtractTextWebpackPlugin('style.css')
+    ]
 }
